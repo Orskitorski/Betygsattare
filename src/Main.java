2511baa99671e0510ab4268408ff668f073f983c
Orskitorski <oscar.tafvelin@elev.ga.ntig.se>
2023-10-18 08:51:32 +0200

    Jag gjorde uppgiften

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String answer = null;
+        Scanner tgb = new Scanner(System.in);
+        System.out.println("Hur många poäng fick du på provet?");
+        int points = tgb.nextInt();
+        tgb.nextLine();
+        if (points <= 30) {
+            if (points < 15) {
+                answer = ("Du fick betyget F");
+            } else if (points > 14 && points < 18 ){
+                answer = ("Du fick betyget E");
+            } else if (points > 17 && points < 21) {
+                answer = ("Du fick betyget D");
+            } else if (points > 20 && points < 24) {
+                answer = ("Du fick betyget C");
+            } else if (points > 23 && points < 27) {
+                answer = ("Du fick betyget B");
+            } else if (points > 26) {
+                answer = ("Du fick betyget A");
+            }
+        } else {
+            answer = ("Fusk!");
+        }
+        System.out.println(answer);
+    }
+}
