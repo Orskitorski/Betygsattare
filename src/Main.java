c79c243959d1ce8b6cb54cac15b7edfc932975d6
Orskitorski <oscar.tafvelin@elev.ga.ntig.se>
2023-10-18 09:04:39 +0200

    Jag snyggade till koden

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,21 @@ public class Main {
         tgb.nextLine();
         if (points >= 0 && points <= 30) {
             if (points < 15) {
-                answer = ("Du fick betyget F");
+                answer = ("F");
             } else if (points > 14 && points < 18 ){
-                answer = ("Du fick betyget E");
+                answer = ("E");
             } else if (points > 17 && points < 21) {
-                answer = ("Du fick betyget D");
+                answer = ("D");
             } else if (points > 20 && points < 24) {
-                answer = ("Du fick betyget C");
+                answer = ("C");
             } else if (points > 23 && points < 27) {
-                answer = ("Du fick betyget B");
+                answer = ("B");
             } else if (points > 26) {
-                answer = ("Du fick betyget A");
+                answer = ("A");
             }
+            System.out.println("Du fick betyget: " + answer);
         } else {
-            answer = ("Fusk!");
+            System.out.println("Fusk!");
         }
-        System.out.println(answer);
     }
 }
